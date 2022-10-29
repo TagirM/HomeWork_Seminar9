a569ab78028b10a77fe93a18626fcca4a8e3c618
Admin <tmurzagaleev@yandex.ru>
2022-10-29 14:29:10 +0700

    Решение задачи 1 <Игра крестики-нолики> из ДЗ№9

---
+++ krestiki.py
@@ -0,0 +1,13 @@
+# Задача. Создайте программу для игры в "Крестики-нолики" при помощи виртуального окружения и PIP.
+
+# Решение задачи:
+# 1. Создаем виртуальное окружение.
+# 2. install library tic_tac_toe
+# 3. Код не пишется, запускается готовая игра через консоль,см.ниже.
+
+# - To run a single player mode, type: (the single player mode is by default)
+# tic_tac_toe --mode s
+
+# - To run a two player mode, type:
+# tic_tac_toe --mode t
+
